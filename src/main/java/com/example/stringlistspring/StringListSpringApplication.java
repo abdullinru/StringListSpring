eed959c4d4e1d8a15d59a47194b1090e8820c2bb
abdullinru <abdullin17021989@gmail.com>
2022-08-05 23:57:23 +0600

    Initial commit

---
+++ src/main/java/com/example/stringlistspring/StringListSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.stringlistspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringListSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StringListSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stringlistspring/StringListSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stringlistspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringListSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
